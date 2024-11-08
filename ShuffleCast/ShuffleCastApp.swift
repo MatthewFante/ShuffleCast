54e960b9630f7a31f3d9540e274b0e425b9b218d
Matthew Fante <matthew.fante@gmail.com>
2024-11-07 21:53:58 -0500

    Initial Commit

---
+++ ShuffleCast/ShuffleCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShuffleCastApp.swift
+//  ShuffleCast
+//
+//  Created by Matthew Fante on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ShuffleCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
